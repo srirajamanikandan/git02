15828c394e58db93cd0c6a25d02b3aa68a8d37f7
srirajamanikandan <srirajamanikandan310@gmail.com>
2022-07-16 12:39:37 +0530

    file

---
+++ src/org/file/CreateFile.java
@@ -0,0 +1,50 @@
+package org.file;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+
+	public static void main(String[] args) throws IOException {
+
+		File file = new File("C:\\Users\\elcot\\Documents\\Manikandan\\raja");
+		
+//		boolean mkdir = file.mkdir();
+//		System.out.println(mkdir);
+		
+//		boolean mkdirs = file.mkdirs();
+//		System.out.println(mkdirs);
+		
+//		boolean a = file.createNewFile();
+//		System.out.println(a);
+		
+//		boolean read = file.canRead();
+//		System.out.println(read);
+		
+//		boolean execute = file.canExecute();
+//		System.out.println(execute);
+		
+//		String[] list = file.list();
+//		
+//		for (String string : list) {
+//		System.out.println(string);	
+//		}
+		
+		
+		File[] files = file.listFiles();
+		
+		for (File file2 : files) {
+			System.out.println(file2);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
